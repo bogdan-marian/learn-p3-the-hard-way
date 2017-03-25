5aa4f548edeedf3746a8ca8083c8002f805fcbc8
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2017-03-26 00:33:29 +0100

    ex23 not tested
    Thre is still something that I have to finish reading

--- 01BasicDrils/ex20.py
+++ 01BasicDrils/ex20.py
@@ -29,5 +29,5 @@ print_a_line(current_line, current_file)
 current_line = current_line + 1
 print_a_line (current_line, current_file)
 
-current_line = current_line + 1
+current_line = current_line + 2
 print_a_line(current_line, current_file)
---
+++ 01BasicDrils/ex23.py
@@ -0,0 +1,17 @@
+import sys
+script, encoding, error = sys.argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_sctring = raw_bytes.decode(encoding, erros=errors)
+
+    print(raw_bytes, "<===>", cooked_sctring)
+    
